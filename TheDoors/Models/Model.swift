ab16a1de9dc75131560fbc1eb347d793b6d7bdb9
Dmitry Solomaha <soldmich1995@gmail.com>
2022-09-04 13:24:00 +0300

    Изменил модель. Добавил заполнение полей для основной части программы через модель.

--- TheDoors/Models/Model.swift
+++ TheDoors/Models/Model.swift
@@ -5,16 +5,31 @@
 //  Created by Admin on 01.09.2022.
 //
 
-import Foundation
+import UIKit
 
-class Model{
-    var doorImgNAme: String
+public struct Doors {
+    //var doorImgNAme: String
     var doorTitle: String
     var doorDescr: String
+}
+
+class Model{
     
-    init(doorImgName:String, doorTitle:String, doorDescr:String){
-        self.doorImgNAme = doorImgName
-        self.doorTitle = doorTitle
-        self.doorDescr = doorDescr
+    var door = [Doors]()
+    
+    init(){
+        data()
+        
+    }
+    
+    func data(){
+        let door1 = Doors(doorTitle: "Front door", doorDescr: "Office")
+        let door2 = Doors(doorTitle: "Front door", doorDescr: "Home")
+        let door3 = Doors(doorTitle: "Front door", doorDescr: "Country house")
+        let door4 = Doors(doorTitle: "Door to yard", doorDescr: "Country house")
+        let door5 = Doors(doorTitle: "Front door", doorDescr: "Flat")
+        self.door = [door1, door2, door3, door4, door5]
+       
     }
 }
+
