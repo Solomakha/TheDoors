c4ef0aaa2d5c44b0ade7f84cb5d505bd455d65e3
Dmitry Solomaha <soldmich1995@gmail.com>
2022-09-05 22:50:38 +0300

    Убрал из контроллера CollectionView

--- TheDoors/Controllers/ViewController.swift
+++ TheDoors/Controllers/ViewController.swift
@@ -6,22 +6,25 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    var collectionView: UICollectionView!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(myView)
     }
-    
+  
     var myView: MainView = {
         let model: Model = Model()
         let rView = MainView(with: model)
         rView.translatesAutoresizingMaskIntoConstraints = false
         return rView
     }()
-    
-    
-}
 
+    override func viewWillLayoutSubviews(){
+        super.viewWillLayoutSubviews()
+    }
+}
