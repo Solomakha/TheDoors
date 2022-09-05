801872db1f32e3e272753cbb36de73a40d146edf
Dmitry Solomaha <soldmich1995@gmail.com>
2022-09-05 22:54:25 +0300

    Создал View в которой буду отображать необходимые окна. Изменил вызов представления CollectionView. Прописал поля которые вызываю из модели и передаю в CollectionView.

--- TheDoors/Views/MainView.swift
+++ TheDoors/Views/MainView.swift
@@ -9,6 +9,15 @@ import UIKit
 import SnapKit
 
 class MainView: UIView {
+   
+    public var myView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .purple
+        view.frame = .zero
+        
+        return view
+    }()
+    
     //Инициализирую картинку с названием программы
     lazy var titleImage: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "Logo"))
@@ -35,31 +44,15 @@ class MainView: UIView {
         label.numberOfLines = 1
         return label
     }()
-    //
-    lazy var doorTitleLabel: UILabel = {
+    //Инициализирую подпись коллекции
+    private var myDoorLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        label.text = "My Door"
+        label.font = UIFont.systemFont(ofSize: 25, weight: .medium)
         label.textAlignment = .left
         label.textColor = .black
         label.numberOfLines = 1
         return label
-    }()
-    //
-    lazy var doorDescrLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 20, weight: .medium)
-        label.textAlignment = .left
-        label.textColor = .gray
-        label.numberOfLines = 1
-        return label
-    }()
-    //
-    lazy var doorStackView: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [doorTitleLabel, doorDescrLabel])
-        stack.axis = .vertical
-        stack.alignment = .leading
-        stack.spacing = 4
-        return stack
     }()
     // Инициализирую кнопку настроек
     private var settingButton: UIButton = {
@@ -72,29 +65,60 @@ class MainView: UIView {
         return settingButton
     }()
     
-    private var collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        let collection = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+    var collectionView: UICollectionView = {
+       let layout = UICollectionViewFlowLayout()
+        let collection = UICollectionView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height), collectionViewLayout: layout)
+        collection.register(CollectionViewCell.self, forCellWithReuseIdentifier: "CollectionViewCell")
         collection.translatesAutoresizingMaskIntoConstraints = false
-       // collection.delegate = self
-       // collection.dataSource = self
         collection.backgroundColor = .gray
-        return collection
+        //collection.scrollToItem(at: IndexPath(item: 1, section: 0), at: .centeredHorizontally, animated: true)
+       return collection
     }()
+    
+    /*
+     var scrollView: UIScrollView = {
+         let scrollView = UIScrollView()
+         scrollView.backgroundColor = .green
+         scrollView.frame = view.bounds
+         scrollView.contentSize = contentSize
+         return scrollView
+     }()
+     var contentView: UIView = {
+         let contentView = UIView()
+         contentView.backgroundColor = .red
+         contentView.frame.size = contentSize
+         return contentView
+     }()
+     var stackView: UIStackView = {
+         let stackView = UIStackView()
+         stackView.axis = .vertical
+         stackView.alignment = .center
+         stackView.spacing = 20
+         return stackView
+     }()
+     
+     public var contentSize: CGSize{
+         CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+     }
+     
+     */
     
     override init (frame: CGRect){
         super.init(frame: frame)
+        collectionView.delegate = self
+        collectionView.dataSource = self
         self.addView()
         self.setConstraint()
-        self.buttonTapped()
-        settingButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+       
+        //settingButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
     }
     public convenience init (with model: Model){
         self.init(frame: .zero)
         //self.imageView.image = UIImage(named: model.doorImgNAme)
-        self.doorTitleLabel.text = model.door[1].doorTitle
-        self.doorDescrLabel.text = model.door[1].doorDescr
-       
+        //self.doorTitleLabel.text = model.door[1].doorTitle
+        //self.doorDescrLabel.text = model.door[1].doorDescr
+        self.buttonTapped()
     }
     
     required init?(coder: NSCoder) {
@@ -103,14 +127,21 @@ class MainView: UIView {
     
     private func addView(){
         addSubview(welcomeLabel)
-        addSubview(doorTitleLabel)
-        addSubview(doorStackView)
-        addSubview(collectionView)
+        addSubview(myView)
         addSubview(settingButton)
         addSubview(titleImage)
         addSubview(houseImageView)
+        addSubview(myDoorLabel)
+        addSubview(collectionView)
+        //addSubview(scrollView)
+        myView.addSubview(collectionView)
+        //scrollView.addSubview(collectionView)
+        //addSubview(stackView)
+        //collectionView.addSubview(stackView)
     }
-     
+    
+   
+    
     private func setConstraint(){
         // Прорисовываю картинку с названием программы
         titleImage.snp.makeConstraints { make in
@@ -139,21 +170,60 @@ class MainView: UIView {
             make.width.equalTo(225)
             make.height.equalTo(200)
         }
-        
-        /*
-        doorStackView.snp.makeConstraints{ make in
-            make.leading.top.equalTo(60)
-            make.centerX.equalTo(welcomeLabel.snp.centerX)
-            make.trailing.equalToSuperview().offset(-20)
+        myDoorLabel.snp.makeConstraints{ make in
+            make.leading.equalToSuperview().inset(20)
+            make.top.equalTo(welcomeLabel.snp.bottom).offset(100)
         }
         
-       
-         */
-        //settingButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        myView.snp.makeConstraints{ make in
+            make.leading.trailing.equalToSuperview().inset(10)
+            make.top.equalTo(myDoorLabel.snp.bottom).offset(5)
+            make.height.equalTo(500)
+        }
+        /*scrollView.snp.makeConstraints{ make in
+            make.top.equalTo(myView.snp.top).inset(5)
+            make.leading.trailing.equalToSuperview().inset(5)
+            make.height.equalToSuperview().inset(5)
+        }
+        */
+        collectionView.snp.updateConstraints{make in
+            make.top.equalTo(myView.snp.top).inset(5)
+            make.leading.trailing.equalToSuperview().inset(5)
+            make.height.equalToSuperview().inset(5)
+        }
+        
+        
+
     }
     //Функция нажатия на кнопку настроек
     @objc private func buttonTapped(){
         print("Функционал в разработке")
     }
     
+    
+}
+
+extension MainView: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let model: Model = Model()
+        return model.door.count
+       
+    }
+    
+    
+func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let model: Model = Model()
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.identifier, for: indexPath) as! CollectionViewCell
+    cell.doorTitleLabel.text = String("\(model.door[indexPath.item].doorTitle)")
+    cell.doorDescrLabel.text = String("\(model.door[indexPath.item].doorDescr)")
+    
+    cell.awakeFromNib()
+    return cell
+}
+   /*
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 300, height: 300)
+    }
+    */
 }
