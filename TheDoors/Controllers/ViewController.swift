be24204f63af14fb3b9ad7b24d03025abaae757d
Dmitry Solomaha <soldmich1995@gmail.com>
2022-09-06 21:55:05 +0300

    Написал функцию нажатия на кнопку настроек.

--- TheDoors/Controllers/ViewController.swift
+++ TheDoors/Controllers/ViewController.swift
@@ -9,22 +9,45 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
-    var collectionView: UICollectionView!
-  
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(myView)
+        view.addSubview(settingButton)
+        
+        // Прорисовываю кнопоку настрек
+        settingButton.snp.makeConstraints{ make in
+            make.width.height.equalTo(50)
+            make.leading.equalTo(myView.titleImage.snp.trailing).offset(200)
+            make.top.equalTo(50)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+        
+        settingButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
   
     var myView: MainView = {
         let model: Model = Model()
         let rView = MainView(with: model)
         rView.translatesAutoresizingMaskIntoConstraints = false
+        
         return rView
     }()
 
-    override func viewWillLayoutSubviews(){
-        super.viewWillLayoutSubviews()
+    // Инициализирую кнопку настроек
+    private var settingButton: UIButton = {
+        let image = UIImage(named: "Setting_img")
+        let settingButton = UIButton(type: .system)
+        settingButton.backgroundColor = .green
+        settingButton.layer.cornerRadius = 15
+        settingButton.setTitle("", for: .normal)
+        settingButton.setImage(image?.withRenderingMode(.alwaysOriginal), for: .normal)
+        return settingButton
+    }()
+    
+    //Функция нажатия на кнопку настроек
+    @objc private func buttonTapped(sender: UIButton){
+        print("Функционал в разработке")
     }
 }
